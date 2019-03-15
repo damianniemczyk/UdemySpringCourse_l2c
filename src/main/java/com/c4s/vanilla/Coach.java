5ba7a42fc442aa8f1599a1e24ff0e9a0379c199c
Damian <damianniemczyk@users.noreply.github.com>
2019-03-15 15:14:10 +0100

    Package refactoring

--- src/main/java/com/c4s/vanilla/Coach.java
+++ src/main/java/com/c4s/vanilla/Coach.java
@@ -1,4 +1,4 @@
-package com.c4s.vanilla.main;
+package main.java.com.c4s.vanilla;
 
 public interface Coach {
     String getDailyWorkout();
--- src/main/java/com/c4s/vanilla/HelloSpringApp.java
+++ src/main/java/com/c4s/vanilla/HelloSpringApp.java
@@ -1,4 +1,4 @@
-package com.c4s.vanilla.main;
+package main.java.com.c4s.vanilla;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
